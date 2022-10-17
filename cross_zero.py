e3ee8919373ff658e0930cf1d936d4da84b22796
Direktor88 <direktorim@yandex.ru>
2022-10-17 22:29:46 +0500

    add cross_zero.py

---
+++ cross_zero.py
@@ -0,0 +1,9 @@
+# [[a,b,c]
+#  [d,e,f]
+#  [g,h,i]]
+# a,b,c; a.e.i; a,d,g; d,c,f; g,h,i; c.e.g; b,e,h; e,f.i
+matrix=[[],[],[]]
+cross=input('vvedi krestik   ')
+zero=input('vvedi nolik   ')
+stroka=input('vvedi stroku 1-3   ')
+kolonka=input('vvedi kolonku 1-3   ')
