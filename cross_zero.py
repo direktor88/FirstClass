ba5f40301a6bdc50bf68a745096300a6bde3400c
Dmitri <115719914+direktor88@users.noreply.github.com>
2022-11-02 23:06:14 +0500

    первоначальное заполнение
    first add item

--- cross_zero.py
+++ cross_zero.py
@@ -5,5 +5,12 @@
 matrix=[[],[],[]]
 cross=input('vvedi krestik   ')
 zero=input('vvedi nolik   ')
+# chto vvedeno?
+
 stroka=input('vvedi stroku 1-3   ')
 kolonka=input('vvedi kolonku 1-3   ')
+x=matrix[0]
+y=matrix[1]
+z=matrix[2]
+if stroka == int('1');
+    
