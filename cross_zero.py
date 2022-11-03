c4c723758b24f42217f26c15d24b0c5b9e54c696
Direktor88 <direktorim@yandex.ru>
2022-11-03 22:26:44 +0500

    proba dict

--- cross_zero.py
+++ cross_zero.py
@@ -1,15 +1,23 @@
-# [[a,b,c]
-#  [d,e,f]
-#  [g,h,i]]
-# a,b,c; a.e.i; a,d,g; d,c,f; g,h,i; c.e.g; b,e,h; e,f.i
-matrix=[[],[],[]]
+#  a,b,c
+#  d,e,f
+#  g,h,i
+# a,b,c; a.e.i; a,d,g; d,c,f; g,h,i; c.e.g; b,e,h; e,f.i win position
+
+matrix={} #dict
+print('select cross or zero' )
+
+#select cross or zero
+stroka=input('vvedi stroku 1-3   ')
+kolonka=input('vvedi kolonku 1-3   ')
 cross=input('vvedi krestik   ')
+
+
 zero=input('vvedi nolik   ')
 # chto vvedeno?
 
 stroka=input('vvedi stroku 1-3   ')
 kolonka=input('vvedi kolonku 1-3   ')
-x=matrix[0]
+c=matrix[0]
 y=matrix[1]
 z=matrix[2]
 if stroka == int('1');
