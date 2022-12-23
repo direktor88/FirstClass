ff4ac885320de7edae471f8e657d603d9107b7d9
Direktor88 <direktorim@yandex.ru>
2022-12-23 23:38:06 +0500

    add any comments

--- cross_zero.py
+++ cross_zero.py
@@ -36,6 +36,8 @@ m=[
 # m[3,1] and m[3,2] and m[3,3]
 # m[3,1] and m[2,2] and m[1,3]
 
+# нужно прописать зависимость
+# https://lms.skillfactory.ru/courses/course-v1:SkillFactory+FPW-2.0+27AUG2020/courseware/5474bc39c2294893840f3e89e37d31db/c9591776961d497bbeb9bad8c5e41685/3?activate_block_id=block-v1%3ASkillFactory%2BFPW-2.0%2B27AUG2020%2Btype%40vertical%2Bblock%40be2d1081789d4a98a9a670f01557594b
 print('-----')
 for i in range (9): # all step = 9
     z=int(input('vvedi stolbec  '))
